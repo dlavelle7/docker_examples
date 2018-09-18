ba4df7c06b1cd824734931e06776f2169e0b3fcd
David Lavelle <davidlavelle1@hotmail.com>
2018-09-18 20:13:14 +0100

    check postgres user and db in entrypoint

--- docker_compose_full_stack/web/entrypoint.sh
+++ docker_compose_full_stack/web/entrypoint.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+# The container is started, but the user & db might not have been created yet
+echo "Wait until web app user & database are created"
+retries="10"
+while [ $retries -gt 0 ]
+do
+    psql -U $DB_USER -d $DB_NAME -h postgres -c "\q"
+    if [ $? -eq 0 ]
+    then
+        echo "Database ready for web app to connect"
+        break
+    else
+        sleep 1
+        let "retries--"
+    fi
+done
+
+if [ $retries -eq 0 ]
+then
+    echo "Database in unhealthy state"
+    exit 1
+fi
+
 echo "Collect static files"
 python3 manage.py collectstatic --noinput
 
