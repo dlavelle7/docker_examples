2bb2a6fdae882e3134d8bfb69f259658205cf5ec
David Lavelle <davidlavelle1@hotmail.com>
2019-02-03 13:39:36 +0000

    trying to connect to es

--- docker_compose_elasticsearch/web/search_engine/country_search/views.py
+++ docker_compose_elasticsearch/web/search_engine/country_search/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+
+
+class SearchEngineConnection(object):
+    """Singleton pattern to ensure only on elasticsearch connection is made."""
+    _connection = None
+    # TODO: Put in settings.py
+    host = "elastic"  # host name of docker container
+
+    def __init__(self):
+        self.connection = self.get_connection()
+
+    @classmethod
+    def get_connection(cls):
+        if SearchEngineConnection._connection is None:
+            SearchEngineConnection._connection = Elasticsearch(
+                cls.host, connection_class=RequestsHttpConnection)
+        return SearchEngineConnection._connection
+
 
 def index(request):
-    context = {}
+    es = SearchEngineConnection().connection
+    res = es.search(index="countries", body={"query": {"match_all": {}}})
+    context = {"countries": res}
     return render(request, 'country_search/index.html', context)
