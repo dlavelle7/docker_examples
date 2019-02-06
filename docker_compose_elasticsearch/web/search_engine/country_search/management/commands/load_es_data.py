e8a0dff091261e60e597a785b4b60a3fc7c1e216
David Lavelle <davidlavelle1@hotmail.com>
2019-02-06 19:39:01 +0000

    flake8

--- docker_compose_elasticsearch/web/search_engine/country_search/management/commands/load_es_data.py
+++ docker_compose_elasticsearch/web/search_engine/country_search/management/commands/load_es_data.py
@@ -1,6 +1,6 @@
 import pycountry
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from country_search.elastic_connection import SearchEngineConnection
 
@@ -16,17 +16,15 @@ class Command(BaseCommand):
             return
         print("Countries index doesn't exist, creating . . .")
         # TODO: Bulk create
-        conn.es.indices.delete(index=conn.countries_index,
-                          ignore=[400, 404])
+        conn.es.indices.delete(index=conn.countries_index, ignore=[400, 404])
         conn.es.indices.create(index=conn.countries_index)
         # generate "country" data for search engine
         for idx, country in enumerate(pycountry.countries):
             country_body = conn.get_country_body(
                 country.name, country.alpha_2, country.alpha_3)
-
-            res = conn.es.index(index=conn.countries_index,
-                           doc_type=conn.country_doc,
-                           id=idx, body=country_body)
+            conn.es.index(
+                index=conn.countries_index, doc_type=conn.country_doc,
+                id=idx, body=country_body)
         # refresh search engine so that data is available
         conn.es.indices.refresh(index=conn.countries_index)
         print("Search engine data populated")
