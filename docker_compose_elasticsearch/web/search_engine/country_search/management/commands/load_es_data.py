c975bea762c8d9a0fba7a33172636d0e81eeb85c
David Lavelle <davidlavelle1@hotmail.com>
2019-02-06 19:26:58 +0000

    refactored manage.py cmd

--- docker_compose_elasticsearch/web/search_engine/country_search/management/commands/load_es_data.py
+++ docker_compose_elasticsearch/web/search_engine/country_search/management/commands/load_es_data.py
@@ -4,30 +4,29 @@ from django.core.management.base import BaseCommand, CommandError
 
 from country_search.elastic_connection import SearchEngineConnection
 
-es = SearchEngineConnection().es
+conn = SearchEngineConnection()
 
 
 class Command(BaseCommand):
     help = 'Populates elasticsearch search engine with data if required.'
 
     def handle(self, *args, **options):
-        if es.indices.exists(index=SearchEngineConnection.countries_index):
+        if conn.es.indices.exists(index=conn.countries_index):
             print("Countries index exists, nothing to do here.")
             return
         print("Countries index doesn't exist, creating . . .")
         # TODO: Bulk create
-        # TODO: Move this to connection.py
-        es.indices.delete(index=SearchEngineConnection.countries_index,
+        conn.es.indices.delete(index=conn.countries_index,
                           ignore=[400, 404])
-        es.indices.create(index=SearchEngineConnection.countries_index)
+        conn.es.indices.create(index=conn.countries_index)
         # generate "country" data for search engine
         for idx, country in enumerate(pycountry.countries):
-            country_body = SearchEngineConnection.get_country_body(
+            country_body = conn.get_country_body(
                 country.name, country.alpha_2, country.alpha_3)
 
-            res = es.index(index=SearchEngineConnection.countries_index,
-                           doc_type=SearchEngineConnection.country_doc,
+            res = conn.es.index(index=conn.countries_index,
+                           doc_type=conn.country_doc,
                            id=idx, body=country_body)
         # refresh search engine so that data is available
-        es.indices.refresh(index=SearchEngineConnection.countries_index)
+        conn.es.indices.refresh(index=conn.countries_index)
         print("Search engine data populated")
